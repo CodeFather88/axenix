0d161ec0a47091b94f3c8948984022e748fff410
nearyouuu <90281230+nearyouuu@users.noreply.github.com>
2023-10-22 10:29:49 +0300

    update handlers_controller

--- controllers/handlers_controller.js
+++ controllers/handlers_controller.js
@@ -10,47 +10,61 @@ export const handle = async (req, res) => {
     let handler_entity = req.body.handler_entity
 
     if (handler_type == 'started the task'){
-        forklifts.updateOne({where: {id: forklift_id, warehouse_id: warehouse_id}}, {
+        let forklifts_upd = await forklifts.updateOne({where: {id: forklift_id, warehouse_id: warehouse_id}}, {
             task_id: handler_entity,
             last_point_id: 1,
             point_id: 1,
             status: 1
         })
-        tasks.updateOne({where: {id: handler_entity, warehouse_id: warehouse_id}}, {
+        let tasks_upd = await tasks.updateOne({where: {id: handler_entity, warehouse_id: warehouse_id}}, {
             forklift_id: forklift_id,
             start_time: new Date(),
             status: 1
         })
+        if (forklifts_upd && tasks_upd){
+            return res.send({
+                state: 1
+            })
+        }
+        return res.code(400).send({ state: 0, message: 'update error' })
     }
     else if (handler_type == 'reach the point'){
-        points.findOne({where: {id: handler_entity}}).then(res => {
+        points.findOne({where: {id: handler_entity}}).then(async res => {
             if (!res){
                 return res.code(404).send({ state: 0, message: 'point_id not found' })
             }
-            forklifts.updateOne({where: {id: forklift_id, warehouse_id: warehouse_id}}, {
+            let forklifts_upd = await forklifts.updateOne({where: {id: forklift_id, warehouse_id: warehouse_id}}, {
                 point_id: handler_entity,
                 status: 1
             })
+            if (forklifts_upd){
+                return res.send({
+                    state: 1
+                })
+            }
+            return res.code(400).send({ state: 0, message: 'update error' })
         })
 
     }
     else if (handler_type == 'finished the task'){
-        forklifts.updateOne({where: {id: forklift_id, warehouse_id: warehouse_id}}, {
+        let forklifts_upd = await forklifts.updateOne({where: {id: forklift_id, warehouse_id: warehouse_id}}, {
             task_id: 0,
             point_id: 1,
             status: 0
         })
-        tasks.updateOne({where: {id: handler_entity, warehouse_id: warehouse_id}}, {
+        let tasks_upd = await tasks.updateOne({where: {id: handler_entity, warehouse_id: warehouse_id}}, {
             end_time: new Date(),
             status: 2
         })
+        if (forklifts_upd && tasks_upd){
+            return res.send({
+                state: 1
+            })
+        }
+        return res.code(400).send({ state: 0, message: 'update error' })
     }
-
-
-    // let data = await result.get({ plain: true })
-    // return res.send({
-    //     data: data,
-    //     state: 1
-    // })
+    else{
+        return res.code(400).send({ state: 0, message: 'unknown handler_type' })
+    }
 }
 
